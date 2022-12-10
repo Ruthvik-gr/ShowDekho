59b90af64a7ea93c6bdec403a8616d0272279436
Ruthvik-gr <grruthvik@gmail.com>
2022-12-10 11:52:49 +0530

    Register and login page

--- app/src/main/java/com/example/showdekho/HomeActivity.java
+++ app/src/main/java/com/example/showdekho/HomeActivity.java
@@ -1,14 +1,26 @@
 package com.example.showdekho;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class HomeActivity extends AppCompatActivity {
+
+    ImageButton plus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        plus = findViewById(R.id.org);
+
+        plus.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplicationContext(), Organizer.class);
+            startActivity(intent);
+        });
     }
 }
